e6ebe50aaf08b52eeca141e3bccb688ed6086543
Mike Shaver <shaver@off.net>
2025-01-19 20:27:19 -0500

    light neovim tweaks

--- modules/home/neovim/default.nix
+++ modules/home/neovim/default.nix
@@ -4,6 +4,9 @@
     flake.inputs.nixvim.homeManagerModules.nixvim
   ];
 
+  programs.neovim.vimAlias = true;
+  programs.neovim.viAlias = true;
+
   programs.nixvim = import ./nixvim.nix // {
     enable = true;
   };
