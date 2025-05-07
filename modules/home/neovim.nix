c0ba0b6693face157f60ddf8377fdc7209e2d820
Mike Shaver <shaver@off.net>
2025-05-07 15:08:19 -0400

    actually enable neovim!

--- modules/home/neovim.nix
+++ modules/home/neovim.nix
@@ -100,6 +100,9 @@
           },
         })
       '';
+
+    enable = true;
+
   };
 
   # https://github.com/nvim-treesitter/nvim-treesitter#i-get-query-error-invalid-node-type-at-position
@@ -116,5 +119,5 @@
     "${parsers}/parser";
 
   # Normal LazyVim config here, see https://github.com/LazyVim/starter/tree/main/lua
-  xdg.configFile."nvim/lua".source = ./lua;
+  # xdg.configFile."nvim/lua".source = ./lua;
 }
