81f760b8bd2a3b8d71d163d4ac217d407136f9ea
Mike Shaver <shaver@off.net>
2025-11-10 11:42:18 -0500

    enable node and python3

--- modules/home/nvim.nix
+++ modules/home/nvim.nix
@@ -13,7 +13,11 @@ in
       viAlias = true;
       vimAlias = true;
       vimdiffAlias = true;
+      withNodeJs = true;
+      withPython3 = true;
   };
+
+  # use the "raw" nvim config from this repo
   xdg.configFile = {
       nvim = {
         source = 
