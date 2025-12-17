82ffe8f96ab1432e27b75ae0a424a7fbd5936dfe
Mike Shaver <shaver@off.net>
2025-12-17 16:48:15 -0500

    basic indent defaults

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,2 +1,11 @@
+-- expand tabs as spaces
+vim.opt.expandtab = true
+-- tab stops and indentation are 4 spaces
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+-- auto indent in a couple of ways
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
