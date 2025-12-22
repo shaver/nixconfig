885e2985c6c6d45944db160350d714e98e76f08c
Mike Shaver <shaver@off.net>
2025-12-22 11:44:09 -0500

    add some mason deps

--- modules/home/nvim.nix
+++ modules/home/nvim.nix
@@ -1,37 +1,39 @@
-{ pkgs
-, lib
-, config
-, ...
+{
+  pkgs,
+  config,
+  ...
 }:
 let
   configRepo = "${config.home.homeDirectory}/nixconfig";
 in
-{ 
-    programs.neovim = {
-      enable = true;
-      defaultEditor = true;
-      viAlias = true;
-      vimAlias = true;
-      vimdiffAlias = true;
-      withNodeJs = true;
-      withPython3 = true;
-      extraPackages = with pkgs; [
-        python3
-        luarocks
-        tree-sitter
-        imagemagick
-        tectonic
-        ghostscript
-        mermaid-cli
-      ];
+{
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+    withNodeJs = true;
+    withPython3 = true;
+    extraPackages = with pkgs; [
+      go
+      python3
+      luarocks
+      tree-sitter
+      imagemagick
+      tectonic
+      ghostscript
+      mermaid-cli
+      statix
+      cargo
+    ];
   };
 
   # use the "raw" nvim config from this repo
   xdg.configFile = {
-      nvim = {
-        source = 
-          config.lib.file.mkOutOfStoreSymlink "${configRepo}/config/nvim";
-        recursive = true;
-      };
+    nvim = {
+      source = config.lib.file.mkOutOfStoreSymlink "${configRepo}/config/nvim";
+      recursive = true;
     };
+  };
 }
